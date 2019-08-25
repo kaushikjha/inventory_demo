5b0fb87f38ef55eb800140bed709c569563e0bce
Kaushik Jha <kanak.kaushik@gmail.com>
2019-08-26 04:49:17 +0530

    added seed data for products using Faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+# Seed product(60 items) for initial setup  in development
+sku_code_init = 10000
+60.times do |i|
+  puts "ISP#{sku_code_init + i}"
+  Product.create(sku_code: "ISP#{sku_code_init + i}", name: Faker::Commerce.product_name, price: Faker::Commerce.price)
+end
