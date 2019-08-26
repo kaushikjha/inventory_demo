2e6f5dbc1d6a6dde1ae13dd106711242fd023345
Kaushik Jha <kanak.kaushik@gmail.com>
2019-08-26 13:04:10 +0530

    product list page should be the root URL

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'warehouses/new'
   get 'products/list'
   get 'products/edit'
+  root to: "products#list"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
