f6288f33c48c6fe7dae424059e0c4e5345a16222
Kaushik Jha <kanak.kaushik@gmail.com>
2019-09-01 13:49:11 +0530

    changes to product edit page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,6 +8,23 @@ class ProductsController < ApplicationController
     @product = Product.includes(:product_warehouses).find(params[:id])
   end
   def update
-
+    @product = Product.find(params[:id])
+    respond_to do |format|
+      if @product.update(product_params)
+        ProductWarehouse.where(product_id: @product.id).update_all(item_count: params[:item_count], low_item_threshold: params[:low_item_threshold])
+        format.html { redirect_to root_path, notice: 'Product was successfully updated.' }
+      else
+        format.html { redirect_to edit_product_path(id: @product.id), notice: 'Product was successfully updated.' }
+      end
+    end
   end
+
+
+
+  private
+
+  def product_params
+    params.require(:product).permit(:sku_code, :name, :price)
+  end
+
 end
